8f6ec9d2806f47e220f7755b236e844fb919e366
yasinkiziltas <kzltasyasin@gmail.com>
2022-04-19 13:17:40 +0300

    Onboard UI

--- src/screens/auth/OnboardScreen.js
+++ src/screens/auth/OnboardScreen.js
@@ -1,89 +1,122 @@
 import React from 'react'
-import { View, StyleSheet, ImageBackground, TouchableOpacity, Text } from 'react-native'
+import { View, Text, Button, StyleSheet, TouchableOpacity, ImageBackground, Image, StatusBar } from 'react-native'
+import Onboarding from 'react-native-onboarding-swiper'
 import * as Animatable from 'react-native-animatable';
-import { onboard2 } from '../../constants/images';
-import { SIZES, COLORS } from '../../constants/theme';
-import FormButton from '../../components/FormButton'
+import { onboard, onboard2, onboard3, onboard4 } from '../../constants/images';
+import { COLORS, SIZES } from '../../constants/theme';
 
+const Dots = ({ selected }) => {
+    let backgroundColor;
+    backgroundColor = selected ? COLORS.bottomColor : 'rgba(0,0,0,0.3)'
+
+    return (
+        <View
+            style={{
+                borderRadius: 5,
+                width: 20,
+                height: 10,
+                marginHorizontal: 5,
+                backgroundColor
+            }}>
+        </View>
+    )
+}
+
+const Skip = ({ ...props }) => {
+    return (
+        <Animatable.View animation="fadeInLeft" delay={10}>
+            <TouchableOpacity {...props} style={styles.btn}>
+                <Text style={styles.btnContainer}>Geç</Text>
+            </TouchableOpacity>
+        </Animatable.View>
+    )
+}
+
+const Next = ({ ...props }) => {
+    return (
+        <Animatable.View animation="fadeInRight" delay={10}>
+            <TouchableOpacity {...props} style={styles.btn}>
+                <Text style={styles.btnContainer}>İleri</Text>
+            </TouchableOpacity>
+        </Animatable.View>
+    )
+}
+
+const Done = ({ ...props }) => {
+    return (
+        <Animatable.View animation="fadeInUp">
+            <TouchableOpacity {...props} style={styles.btn}>
+                <Text style={styles.btnDone}>Başla!</Text>
+            </TouchableOpacity>
+        </Animatable.View>
+
+    )
+}
 export default function OnboardScreen({ navigation }) {
     return (
         <>
-            <Animatable.View
-                animation="slideInDown"
-                style={styles.container}
-            >
-                <ImageBackground
-                    source={onboard2}
-                    style={styles.image}
+            <StatusBar hidden={true} />
+
+            <Animatable.View style={{ flex: 1 }} animation="fadeInUp">
+                <Onboarding
+                    onSkip={() => navigation.replace('Login')}
+                    onDone={() => navigation.navigate('Login')}
+                    SkipButtonComponent={Skip}
+                    NextButtonComponent={Next}
+                    DoneButtonComponent={Done}
+                    DotComponent={Dots}
+                    bottomBarHeight={70}
+
+                    pages={[
+                        {
+                            backgroundColor: '#7FCDCD',
+                            image:
+                                <Image
+                                    source={onboard3}
+                                    style={{
+                                        width: 450,
+                                        height: 400,
+                                    }}
+                                />,
+                            titleStyles: { fontWeight: 'bold' },
+                            title: "Akor' a hoşgeldin!",
+                            subtitle: 'Sahne arkadaşını bul,',
+                        },
+
+                        {
+                            backgroundColor: '#F0FFF0', //feeae6
+                            image:
+                                <Image
+                                    source={onboard4}
+                                    style={{
+                                        borderRadius: 30,
+                                        width: 450,
+                                        height: 400,
+                                    }}
+                                />,
+                            titleStyles: { fontWeight: 'bold' },
+                            title: "Akor",
+                            subtitle: 've sahnene davet et!'
+                        },
+
+                    ]}
                 />
             </Animatable.View>
 
-            <Animatable.View
-                animation="slideInUp"
-                style={{ flexDirection: 'column' }}
-            >
-                <Text style={styles.onboardText}>HOŞGELDİN!</Text>
-                <Text style={styles.onboardTabText}>Sahneni veya sahne arkadaşını bulmak için en ideal yer!</Text>
-
-                <View style={styles.buttonContainer}>
-                    <FormButton
-                        text="Giriş"
-                        onPress={() => navigation.navigate('Login')}
-                        backgroundColor={COLORS.appColor}
-                    />
-
-                    <FormButton
-                        text="Kayıt Ol"
-                        onPress={() => navigation.navigate('Register')}
-                        backgroundColor={COLORS.appColor}
-                    />
-
-                    <FormButton
-                        text="Mekan Sahibi Girişi"
-                        onPress={() =>  navigation.navigate('OwnerAuth')}
-                        backgroundColor={COLORS.appColor}
-                    />
-
-                    <TouchableOpacity
-                        style={{ marginVertical: 50 }}
-                        onPress={() => alert('Non Auth')}>
-                        <Text style={{ color: 'gray', fontWeight: 'bold' }}>Üye olmadan devam et</Text>
-                    </TouchableOpacity>
-                </View>
-            </Animatable.View >
         </>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#e3e4e8'
+    btn: {
+        margin: 20
     },
-    image: {
-        width: SIZES.width,
-        height: SIZES.height / 3,
-        borderWidth: 0.5,
-        borderColor: 'gray',
+    btnContainer: {
+        fontSize: 18,
+        color: 'black'
     },
-    onboardText: {
-        marginVertical: SIZES.width / 20,
-        fontSize: 25,
-        textAlign: 'center',
-        fontWeight: 'bold'
-    },
-    onboardTabText: {
-        textAlign: 'center',
-        color: 'gray',
-        fontSize: 14,
-    },
-    buttonContainer: {
-        marginVertical: 35,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 17
+    btnDone: {
+        fontSize: 18,
+        color: 'black'
     }
 })
